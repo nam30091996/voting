e62f221d7a875ed9d24e35f52207be742c832376
nam30091996 <nguyenphuongnam.ptit@gmail.com>
2018-10-22 23:32:51 +0700

    Initial commit

---
+++ contracts/Voting.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/**
+ * The contractName contract does this and that...
+ */
+contract Voting {
+
+	string public candidate;
+
+	function Voting () public {
+		candidate = "Candidate 1";
+	}	
+}
